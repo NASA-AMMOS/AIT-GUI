d1d99417b882b50546afc8987a57478836302063
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2016-11-22 17:18:01 -0800

    Issue #12 - Initial CSS bundling work
    All CSS is bundled into bliss.bundle.css. At the moment, "all CSS" is
    defined as the index_test.css file and any other css files that it
    imports which is only bootstrap's css for now.
    
    The webpack loaders have been updated to handle all of the
    icon/font/whatnot that's included in bootstrap (those are what all the
    extra build files are). This needs to be explored further. Testing on
    whether the font stuff works hasn't been checked yet.
    
    The example website has been updated to include some CSS styling (the
    giant blue "header") and some bootstrap styling (the full page width
    input box).

--- bliss/gui/static/webpack.config.js
+++ bliss/gui/static/webpack.config.js
@@ -1,7 +1,21 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 module.exports = {
     entry: "./index_test.js",
     output: {
         path: __dirname + '/build',
         filename: "bliss.bundle.js"
     },
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
+            {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("bliss.bundle.css")
+    ]
 };
