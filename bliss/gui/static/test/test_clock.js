8a3fc2e00b84e348badb26444e97460922301a3b
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2017-02-22 11:07:00 -0800

    Add some more clock test examples
    We may need to find a nice way of interacting with the generated DOM
    from the views and "mocking" vnode objects. Something like the below
    repo might be helpful for interacting with the generated DOM for tests.
    
    https://github.com/StephanHoyer/mithril-query

--- bliss/gui/static/test/test_clock.js
+++ bliss/gui/static/test/test_clock.js
@@ -13,4 +13,18 @@ describe('Clock object', function () {
         clock.toggleH24()
         clock._h24.should.equal(false)
     })
+
+    it('should allow for toggling to/from UTC time', function() {
+        var clock = bliss.format.Clock
+        clock._utc.should.equal(true)
+        clock.toggleUTC()
+        clock._utc.should.equal(false)
+    })
+
+    it('should allow for toggling Julian date to/from DOY', function() {
+        var clock = bliss.format.Clock
+        clock._doy.should.equal(false)
+        clock.toggleDOY()
+        clock._doy.should.equal(true)
+    })
 })
