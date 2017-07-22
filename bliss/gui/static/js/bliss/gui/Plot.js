0f2eaf04529905a0dc40fce196076333ec4a9a23
Ben Bornstein <ben.bornstein@jpl.nasa.gov>
2017-07-22 11:46:55 -0700

    Issue #159 - Select time axis telemetry in plots (<bliss-plot-time>)
    A <bliss-plot> used the time telemetry was received by the client GUI
    (i.e. Ground Receipt Time in the case of realtime data) for the time
    axis in plots.  Users are now able to select the telemetry point to
    use for the time axis, per plot, e.g.:
    
        <bliss-plot title="PCE Voltage Groups (A-E)" y-title="Volts">
            <bliss-plot-time packet="OCO3_1553_EHS" name="AnalogsSampleTime"></bliss-plot-time>
            ...
        </bliss-plot>
    
    If the `<bliss-plot-time>` is not specified or the packet or field
    name reference is invalid (i.e. does not exist or when decoded is not
    a number or Javascript `Date`), the current time is used as a
    fallback.
    
    Resolves #159.

--- bliss/gui/static/js/bliss/gui/Plot.js
+++ bliss/gui/static/js/bliss/gui/Plot.js
@@ -4,6 +4,7 @@ import Highcharts from 'highcharts/highstock';
 // See https://github.com/highcharts/highcharts/issues/4994
 window.Highcharts = Highcharts
 
+
 const Plot =
 {
     /**
@@ -18,11 +19,19 @@ const Plot =
             const series = this._chart.get(pname + '.' + name)
 
             if (series) {
-                const x = Date.now()
+                const x = this._time.get(packet)
                 const y = packet.__get__(name)
                 series.addPoint([x, y], true, series.data.length > 600)
             }
         })
+    },
+
+
+    /**
+     * Process tag: `<bliss-plot-time packet="..." name="...">`.
+     */
+    processTagTime (vnode) {
+        this._time = new PlotTimeField(vnode.attrs.packet, vnode.attrs.name)
     },
 
 
@@ -56,9 +65,13 @@ const Plot =
             }
         }
         this._packets = { }
+        this._time    = null
 
         vnode.children.forEach( (child) => {
-            if (child.tag.startsWith('bliss-plot-')) {
+            if (child.tag === 'bliss-plot-time') {
+                this.processTagTime(child)
+            }
+            else if (child.tag.startsWith('bliss-plot-')) {
                 const name   = child.attrs.name
                 const packet = child.attrs.packet
                 const type   = child.tag.substr(11)
@@ -79,6 +92,10 @@ const Plot =
             }
         })
 
+        if (this._time === null) {
+            this._time = new PlotTimeField()
+        }
+
         bliss.events.on('bliss:tlm:packet', (p) => this.plot(p))
     },
 
@@ -92,6 +109,60 @@ const Plot =
     // Mithril lifecycle method
     view (vnode) {
         return m('bliss-plot', vnode.attrs)
+    }
+}
+
+
+/**
+ * PlotTimeField
+ *
+ * A :class:`PlotTimeField` identifies a packet and constituent field
+ * (by name) to use for the the time axis in BLISS plots.  If the
+ * packet name, field name, or resulting field value are invalid
+ * (e.g. not a number or Javascript :class:`Date`), the current time
+ * will be used.
+ *
+ * An empty :class:`PlotTimeField` will always return `Date.now()`, i.e.:
+ *
+ * .. code-block:: javascript
+ *
+ *     new PlotTimeField().get(packet) === Date.now()
+ */
+class PlotTimeField
+{
+    /**
+     * Creates a new :class:`PlotTimeField` with the given packet name
+     * and field name.
+     */
+    constructor(pname=null, fname=null) {
+        this._pname = pname
+        this._fname = fname
+    }
+
+    /**
+     * @returns the plot time for the current BLISS packet or Date.now().
+     */
+    get (packet) {
+        let time = this.hasTime(packet) ? packet.__get__(this._fname) : null
+
+        if (time instanceof Date) {
+            time = time.getTime()
+        }
+        else if (typeof time !== 'number') {
+            time = Date.now()
+        }
+
+        return time
+    }
+
+    /**
+     * @returns ``true`` or ``false`` to indicate whether or not the
+     * given packet has the field referenced by this
+     * :class:`PlotTimeField`.
+     */
+    hasTime (packet) {
+        const defn = packet && packet._defn
+        return defn && this._pname === defn.name && this._fname in defn.fields
     }
 }
 
