fef01d69ec6dba622c77d3ee3ca0410101a334a5
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2017-10-31 08:36:18 -0700

    Prep for 0.13.0 release

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ class CustomInstallCmd(install):
 
 setup(
     name = 'bliss-gui',
-    version = '0.12.0',
+    version = '0.13.0',
     packages = find_packages(exclude=['tests']),
     author = 'BLISS-Core Development Team',
     author_email = 'bliss@jpl.nasa.gov',
@@ -84,7 +84,7 @@ setup(
     namespace_packages   = ['bliss'],
     include_package_data = True,
 
-    install_requires = ['bliss-core>=0.25.0'],
+    install_requires = ['bliss-core>=0.26.0'],
     dependency_links = [
        'https://bliss.jpl.nasa.gov/pypi/simple/bliss-core/'
     ],
