e9a44c8f223d9f842be125897911edbb7f894e5c
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2018-02-15 10:14:47 -0800

    Prep for 0.20.1 release

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ class CustomInstallCmd(install):
 
 setup(
     name = 'bliss-gui',
-    version = '0.20.0',
+    version = '0.20.1',
     packages = find_packages(exclude=['tests']),
     author = 'BLISS-Core Development Team',
     author_email = 'bliss@jpl.nasa.gov',
