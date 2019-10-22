d8735d65b012fbd1c61d3f6bbcd121d731317fd1
Anna Waldron <anna.waldron@jpl.nasa.gov>
2019-10-22 11:21:09 -0700

    Add tracking of dntoeu states on frontend (#105)

--- ait/gui/static/js/ait/gui/Field.js
+++ ait/gui/static/js/ait/gui/Field.js
@@ -83,7 +83,13 @@ const Field =
      * retrieving the packet value.
      */
     getValue (packet, raw=false) {
-        return packet && packet[this._fname]
+        if ( !raw ) {
+            if ( packet && this._fname in packet['dntoeu']) {
+                return packet && packet['dntoeu'][this._fname]
+            }
+        }
+
+        return packet && packet['raw'][this._fname]
     },
 
 
