2f4f262cbdc4501e47ffa4390ef0aa88ec7d72c4
Nicholas Toole <Nicholas.Toole@jpl.nasa.gov>
2020-05-11 10:16:21 -0700

    Preparation for 2.1.0 release

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ class CustomInstallCmd(install):
 
 setup(
     name = 'ait-gui',
-    version = '2.0.0',
+    version = '2.1.0',
     description = description,
     long_description = long_description,
     long_description_content_type = 'text/x-rst',
